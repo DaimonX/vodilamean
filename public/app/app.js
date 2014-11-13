da58d1c433165e4da03a125ad6072d4e9a880d55
DaimonX <dima.volsky@gmail.com>
2014-11-13 22:20:38 +0200

    UI main.jade

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,10 @@
+'use strict'
 angular.module('app', ['ngResource', 'ngRoute']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(false);
   $routeProvider
-    .when('/', { templateUrl: '/partials/main', controller: 'mainCtrl'});
+  .when('/', { templateUrl: '/partials/main', controller: 'mainCtrl'});
 
 });
 
--- server.js
+++ server.js
@@ -27,6 +27,8 @@ app.use(stylus.middleware(
 //all public req will be responded by public dir now.
 app.use(express.static(__dirname + '/public'));
 
+
+// MongoDB configuration
 if(env ==='development'){
 	mongoose.connect('mongodb://localhost:27017/vodilamean', function(err, res) {
  		if(err) {
@@ -45,21 +47,18 @@ if(env ==='development'){
 });
 }
 
-// MongoDB configuration
-
-
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console,'connection error...'));
 db.once('open', function callback(){
 	console.log('vodilamean db opened');
 });
-var messageSchema = mongoose.Schema({message:String});
+
+/*var messageSchema = mongoose.Schema({message:String});
 var Message = mongoose.model('Message', messageSchema);
 var mongoMessage;
 Message.findOne().exec(function(err, messageDoc){
 	mongoMessage = messageDoc.message;
-});
+});*/
 
 //load route
 app.get('/partials/:partialPath', function(req,res){
@@ -67,10 +66,7 @@ app.get('/partials/:partialPath', function(req,res){
 })
 
 app.get('*', function(req,res){
-	res.render('index',{
-		mongoMessage:mongoMessage
-	});
-
+	res.render('index');
 });
 
 //start listening on server
