cc372c7d8e9648e71483aabb8a88958ea40eab9b
DaimonX <dima.volsky@gmail.com>
2014-11-14 09:30:38 +0200

    After complete GUI

--- public/app/app.js
+++ public/app/app.js
@@ -4,10 +4,6 @@ angular.module('app', ['ngResource', 'ngRoute']);
 angular.module('app').config(function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(false);
   $routeProvider
-  .when('/', { templateUrl: '/partials/main', controller: 'mainCtrl'});
+  .when('/', { templateUrl: '/partials/main', controller: 'mvMainCtrl'});
 
-});
-
-angular.module('app').controller('mainCtrl', function($scope) {
-  $scope.myVar = " То чувстов когда заебашил хеловворлд";
 });
---
+++ public/app/main/mvMainCtrl.js
@@ -0,0 +1,13 @@
+angular.module('app').controller('mvMainCtrl', function($scope) {
+  $scope.cars = [
+  	{name:'Toyota', number:'ВХ5567АЕ', fautured: true, published: new Date('21/05/2014')},
+  	{name:'Toyota', number:'ВХ5567АЕ', fautured: true, published: new Date('21/05/2014')},
+  	{name:'Toyota', number:'ВХ5567АЕ', fautured: true, published: new Date('21/05/2014')},
+  	{name:'Toyota', number:'ВХ5567АЕ', fautured: true, published: new Date('21/05/2014')},
+  	{name:'Toyota', number:'ВХ5567АЕ', fautured: true, published: new Date('21/05/2014')},
+  	{name:'Toyota', number:'ВХ5567АЕ', fautured: true, published: new Date('21/05/2014')},
+  	{name:'Toyota', number:'ВХ5567АЕ', fautured: true, published: new Date('21/05/2014')},
+  	{name:'Toyota', number:'ВХ5567АЕ', fautured: true, published: new Date('21/05/2014')},
+  	{name:'Toyota', number:'ВХ5567АЕ', fautured: true, published: new Date('21/05/2014')},
+  ]
+});
